195c45b6fe3d158dc4575fee53e0695b59ee33b5
NovaHavoc <isaacjespinoza32@gmail.com>
2025-03-25 19:18:27 -0400

    Add files via upload

---
+++ games/bloodstrike/script.lua
@@ -0,0 +1,241 @@
+--  Reverse Ι͠ool by @NEOvrj  --- 
+
+
+function L0_41(A0_76, A1_77)
+me = ""
+for sn5 in pairs(A0_76) do
+me = me .. .((A0_76[sn5] + A1_77) % 333)
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+return me
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+
+function L12_53()
+_ENV["gg"]["setVisible"](false)
+_ENV["gg"]["toast"]("NIGGARDLY BLOOD STRIKE SCRIPT")
+menu = _ENV["gg"]["choice"]({
+"  [BYPASS🛡️]",
+"  [MB AND RECOIL🎯]      [MENU]",
+"  [BODY COLOR🎨]          [MENU]",
+"  [ANTENA🗼]",
+"  [HEADSHOT🤕]",
+"  [EXIT❌]"}, nil, " [NiggaSTORYY SCRIPT BLOOD STRIKE]\n [TELEGRAM @NiggaSTORYY]")
+if menu == nil then
+return
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+if menu == 1 then
+byp()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+if menu == 2 then
+mbr()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+if menu == 3 then
+bc()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+if menu == 4 then
+ant()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+if menu == 5 then
+hd()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+if menu == 6 then
+exit()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+
+function L12_53()
+_ENV["gg"]["setVisible"](false)
+colorChoice = _ENV["gg"]["choice"]({"  WHITE BODY & SKY🎨","  [BACK🔙] "}, nil, " [BODY COLOR]\n [@NiggaSTORYY")
+if colorChoice == 1 then
+colorputihv2()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+if colorChoice == 2 then
+home()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+
+function L12_53()
+_ENV["gg"]["setVisible"](false)
+mb = _ENV["gg"]["choice"]({"  NO RECOIL🔰","  BT HEAD BRUTAL🔰","  MB BODY🔰","  MB HEAD SAFE🔰","  [BACK🔙] "}, nil, " [MAGIC BULLET]\n [@NiggaSTORYY]")
+if mb == 1 then
+rec()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+if mb == 2 then
+mbh()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+if mb == 3 then
+mbb()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+if mb == 4 then
+mbaim()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+if mb == 5 then
+home()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+
+function L12_53()
+_ENV["gg"]["alert"]("[0.1-0.6]\nGAY CHETOS\nGAY NIGGARDLY\n\nGAY ZORA\nPRESS OK")
+_ENV["gg"]["clearResults"]()
+if _ENV["gg"]["prompt"]({"[Enter the value for MB HEAD]\n[SAFE 100%]"}, {"0.6"}) and _ENV["gg"]["prompt"]({"[Enter the value for MB HEAD]\n[SAFE 100%]"}, {"0.6"})[1] then
+_ENV["gg"]["setRanges"](_ENV["gg"]["REGION_C_ALLOC"])
+_ENV["gg"]["searchNumber"]("0.11999999732", _ENV["gg"]["TYPE_FLOAT"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["refineNumber"]("0.11999999732", _ENV["gg"]["TYPE_FLOAT"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["getResults"](500, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["editAll"](to(_ENV["gg"]["prompt"]({"[Enter the value for MB HEAD]\n[SAFE 100%]"}, {"0.6"})[1]), _ENV["gg"]["TYPE_FLOAT"])
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["toast"]("magic head + aim")
+_ENV["gg"]["clearResults"]()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+
+function L12_53()
+_ENV["gg"]["clearResults"]()
+if _ENV["gg"]["prompt"]({"[Enter the value for MB BODY]\n[SAFE 100%]"}, {"0.6"}) and _ENV["gg"]["prompt"]({"[Enter the value for MB BODY]\n[SAFE 100%]"}, {"0.6"})[1] then
+_ENV["gg"]["setRanges"](_ENV["gg"]["REGION_C_ALLOC"])
+_ENV["gg"]["searchNumber"]("0.0700000003", _ENV["gg"]["TYPE_FLOAT"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["refineNumber"]("0.0700000003", _ENV["gg"]["TYPE_FLOAT"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["getResults"](9999, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["editAll"](to(_ENV["gg"]["prompt"]({"[Enter the value for MB BODY]\n[SAFE 100%]"}, {"0.6"})[1]), _ENV["gg"]["TYPE_FLOAT"])
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["toast"]("mb body on")
+_ENV["gg"]["setVisible"](false)
+_ENV["gg"]["clearResults"]()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+
+function L12_53()
+_ENV["gg"]["clearResults"]()
+if _ENV["gg"]["prompt"]({"[Enter the value for MB HEAD]\n[SUPER RISKY⚠️]"}, {"999999999"}) and _ENV["gg"]["prompt"]({"[Enter the value for MB HEAD]\n[SUPER RISKY⚠️]"}, {"999999999"})[1] then
+_ENV["gg"]["setRanges"](_ENV["gg"]["REGION_C_ALLOC"])
+_ENV["gg"]["searchNumber"]("0.11999999732", _ENV["gg"]["TYPE_FLOAT"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["refineNumber"]("0.11999999732", _ENV["gg"]["TYPE_FLOAT"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["getResults"](500, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["editAll"](to(_ENV["gg"]["prompt"]({"[Enter the value for MB HEAD]\n[SUPER RISKY⚠️]"}, {"999999999"})[1]), _ENV["gg"]["TYPE_FLOAT"])
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["toast"]("bt head")
+_ENV["gg"]["clearResults"]()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+
+function L12_53()
+_ENV["gg"]["sleep"](100)
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["setRanges"](_ENV["gg"]["REGION_C_ALLOC"])
+_ENV["gg"]["searchNumber"]("1.03499996662", _ENV["gg"]["TYPE_FLOAT"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["refineNumber"]("1.03499996662", _ENV["gg"]["TYPE_FLOAT"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["getResults"](1000, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["editAll"]("0", _ENV["gg"]["TYPE_FLOAT"])
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["setRanges"](_ENV["gg"]["REGION_C_ALLOC"])
+_ENV["gg"]["searchNumber"]("1.505", _ENV["gg"]["TYPE_FLOAT"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["refineNumber"]("1.505", _ENV["gg"]["TYPE_FLOAT"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["getResults"](1000, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["editAll"]("1.035.0", _ENV["gg"]["TYPE_FLOAT"])
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["toast"]("no recoil]")
+_ENV["gg"]["setVisible"](false)
+_ENV["gg"]["clearResults"]()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+
+function L12_53()
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["setRanges"](_ENV["gg"]["REGION_CODE_APP"])
+_ENV["gg"]["searchNumber"]("2044508324", _ENV["gg"]["TYPE_DWORD"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["addListItems"]({})
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["searchNumber"]("2125561021", _ENV["gg"]["TYPE_DWORD"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["addListItems"]({})
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["searchNumber"]("2013776290", _ENV["gg"]["TYPE_DWORD"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["addListItems"]({})
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["searchNumber"]("2137656763", _ENV["gg"]["TYPE_DWORD"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["addListItems"]({})
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["searchNumber"]("2053790376", _ENV["gg"]["TYPE_DWORD"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["setRanges"](_ENV["gg"]["REGION_CODE_APP"])
+_ENV["gg"]["setRanges"](_ENV["gg"]["REGION_BAD"] | _ENV["gg"]["REGION_CODE_APP"])
+_ENV["gg"]["setRanges"](_ENV["gg"]["REGION_CODE_APP"])
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["addListItems"]({})
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["searchNumber"]("2097651377", _ENV["gg"]["TYPE_DWORD"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["addListItems"]({})
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["searchNumber"]("2075208622", _ENV["gg"]["TYPE_DWORD"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["addListItems"]({})
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["searchNumber"]("2094854071", _ENV["gg"]["TYPE_DWORD"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["getResults"](10, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["addListItems"]({})
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["toast"]("Bypass Done")
+_ENV["gg"]["setVisible"](false)
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+
+function L12_53()
+_ENV["gg"]["setRanges"](_ENV["gg"]["REGION_C_ALLOC"])
+_ENV["gg"]["searchNumber"]("0.11999999732", _ENV["gg"]["TYPE_FLOAT"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+_ENV["gg"]["refineNumber"]("0.11999999732", _ENV["gg"]["TYPE_FLOAT"], false, _ENV["gg"]["SIGN_EQUAL"], 0, -1, 0)
+revert = _ENV["gg"]["getResults"](500, nil, nil, nil, nil, nil, nil, nil, nil)
+_ENV["gg"]["editAll"]("0.85999999732", _ENV["gg"]["TYPE_FLOAT"])
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["toast"]("Head Shoot Activate")
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+
+function L12_53()
+_ENV["gg"]["alert"]("IF BUG CHANGE UR GRAPHIC TO LOW")
+_ENV["gg"]["setRanges"](_ENV["gg"]["REGION_C_ALLOC"])
+_ENV["gg"]["searchNumber"]("3.5", 16, false, _ENV["gg"]["SIGN_EQUAL"], 0, -1)
+for sn5 = 1, _ENV["gg"]["getResultsCount"]() do
+if _ENV["gg"]["getResults"]((_ENV["gg"]["getResultsCount"]()))[sn5].flags == _ENV["gg"]["TYPE_FLOAT"] then
+_ENV["gg"]["getResults"]((_ENV["gg"]["getResultsCount"]()))[sn5].value = 9999999999999920
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+_ENV["gg"]["setValues"]((_ENV["gg"]["getResults"]((_ENV["gg"]["getResultsCount"]()))))
+_ENV["gg"]["toast"]("WHITE BODY ON✅]")
+_ENV["gg"]["clearResults"]()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+
+function L12_53()
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["alert"]("RECOMMEND IF U USE THIS JUST RELOG AFTER 1 MATCH\n[Press GameGuardian and press the box in the top left corner, then select interrupt]")
+_ENV["gg"]["clearResults"]()
+_ENV["gg"]["setRanges"](4)
+_ENV["gg"]["searchNumber"]("1.35216355324", 16, false, 536870912, 0, -1, 0)
+_ENV["gg"]["processResume"]()
+_ENV["gg"]["refineNumber"]("1.35216355324", 16, false, 536870912, 0, -1, 0)
+for sn4, sn5 in ((_ENV["gg"]["getResults"](1000))) do
+_ENV["gg"]["editAll"]("-9999999999", 16)
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+_ENV["gg"]["searchNumber"]("0.58781325817", 16, false, 536870912, 0, -1, 0)
+_ENV["gg"]["processResume"]()
+_ENV["gg"]["refineNumber"]("0.58781325817", 16, false, 536870912, 0, -1, 0)
+for sn4, sn5 in ((_ENV["gg"]["getResults"](1000))) do
+_ENV["gg"]["editAll"]("9999999999", 16)
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+_ENV["gg"]["sleep"](1000)
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+
+function L12_53()
+_ENV["gg"]["alert"]("TELEGRAM SEND FEEDBACK HERE @NiggardlyPBSBOT || Thank For Using My Script")
+_ENV["gg"]["skipRestoreState"]()
+_ENV["gg"]["setVisible"](true)
+_ENV["os"]["exit"]()
+end ----𝑫𝒆𝒄 𝑩𝒚 @𝑵𝑬𝑶𝒗𝒓𝒋 @𝑵𝑬𝑶_𝑫𝑬𝑪
+
